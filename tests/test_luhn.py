c43dcf0376cb357dd18263b996ab2f033c357de2
wwxihan2 <zl3499@nyu.edu>
2023-11-07 16:48:33 -0500

    update 3 tests about generate_luhn

--- tests/test_luhn.py
+++ tests/test_luhn.py
@@ -1,23 +1,23 @@
-from luhnchecker.luhn import Luhn
 import pytest
+from luhnchecker.luhn import Luhn
 
 
-def test_generate_luhn_valid_length():
-    for length in range(13, 20):
-        card_number = Luhn.generate_luhn(length=length)
-        # len(card_number) == length checks if the length of the generated number is as requested.
-        # card_number.isdigit() ensures that the generated number contains only digit characters.
-        assert len(card_number) == length and card_number.isdigit()
+class Tests:
+    def test_generate_luhn_valid_length(self):
+        for length in range(13, 20):
+            card_number = Luhn.generate_luhn(length=length)
+            # Check if the length of the generated number is as requested.
+            assert len(card_number) == length, f"Expected length {length}, but got {len(card_number)}"
+            # Ensure that the generated number contains only digit characters.
+            assert card_number.isdigit(), f"Generated card number '{card_number}' contains non-digit characters"
 
+    def test_generate_luhn_invalid_length(self):
+        with pytest.raises(ValueError) as excinfo:
+            Luhn.generate_luhn(length=10)
+        assert "Card number length must be between 13 and 19" in str(excinfo.value), "ValueError does not contain the correct message"
 
-def test_generate_luhn_invalid_length():
-    with pytest.raises(ValueError):
-        Luhn.generate_luhn(length=10)
-
-
-# This test generates a card number and then verifies it with the check_luhn method.
-# If check_luhn returns True, the test passes.
-def test_generate_luhn_passes_check():
-    card_number = Luhn.generate_luhn()
-    assert Luhn.check_luhn(card_number)
-
+    def test_generate_luhn_passes_check(self):
+        card_number = Luhn.generate_luhn()
+        is_valid = Luhn.check_luhn(card_number)
+        # If check_luhn returns True, the test passes. If not, print the card number.
+        assert is_valid, f"Generated card number {card_number} failed the Luhn check"
